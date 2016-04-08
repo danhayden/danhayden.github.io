3eb1fb6ef18ad428541735bc0cb6e0446f72874a
Dan Hayden <pythonic1@gmail.com>
2016-04-08 11:31:18 +0100

    chore: optimise generated css output file size by removing unnecessary prefixes

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -3,6 +3,10 @@ module.exports = {
     'autoprefixer',
     'cssnano'
   ],
+  autoprefixer: {
+    browsers: '> 1%, not Explorer < 11',
+    flexbox: false
+  },
   cssnano: {
     'discardComments': {
       'removeAll': true
